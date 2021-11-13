94321544274c2e5eec8f23d68d954ae10b57f881
Erin Knapp <erin.knapp@gmail.com>
2021-11-12 22:13:24 -0500

    refactoring

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,11 @@
-var formEl = document.querySelector("#task-form");
+vvar formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-<<<<<<< HEAD
 var createTaskHandler = function (event) {
-=======
-var taskFormHandler = function(event) {
->>>>>>> feature/add-task
   event.preventDefault();
   var taskNameInput = document.querySelector("input[name='task-name']").value;
   var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-<<<<<<< HEAD
-=======
-// check if input values are empty strings
-if (!taskNameInput || !taskTypeInput) {
-    alert("You need to fill out the task form!");
-    return false;
-  }
-  formEl.reset();
-
-  // package up data as an object
-  var taskDataObj = {
-      name: taskNameInput,
-      type: taskTypeInput
-  };
-
-  // send it as an argument to createTaskEl
-  createTaskEl(taskDataObj);
-};
-
-var createTaskEl = function (taskDataObj) {
->>>>>>> feature/add-task
   // create list item
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
@@ -38,21 +13,13 @@ var createTaskEl = function (taskDataObj) {
   // create div to hold task info and add to list item
   var taskInfoEl = document.createElement("div");
   taskInfoEl.className = "task-info";
-<<<<<<< HEAD
 
   // add HTML content to div
   taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
-=======
-  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
->>>>>>> feature/add-task
   listItemEl.appendChild(taskInfoEl);
 
   // add entire list item to list
   tasksToDoEl.appendChild(listItemEl);
 };
 
-<<<<<<< HEAD
 formEl.addEventListener("submit", createTaskHandler);
-=======
-formEl.addEventListener("submit", taskFormHandler);
->>>>>>> feature/add-task
